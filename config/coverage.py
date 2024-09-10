3245b08cd9f6dfccf4d2e6b9913400b71a874ed9
yogyagamage <47789154+yogyagamage@users.noreply.github.com>
2024-09-10 09:19:00 -0400

    Fix string

--- config/coverage.py
+++ config/coverage.py
@@ -39,7 +39,7 @@ def main(jacocoCsvFile):
     coverage, branchCoverage = computeCoverage([jacocoCsvFile])
     
     # Return coverage percentage to check against the threshold
-    return round(coverage * 100, 2)
+    return str(round(coverage * 100, 2))
 
 if __name__ == "__main__":
     import sys
